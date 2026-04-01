0d59b4c3848f965fb48628d652327745c81178e7
ayushraj706 <ayushrajayushhh@gmail.com>
2026-04-01 23:09:23 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // Firebase Config
+    firebase: {
+        apiKey: "AIzaSyCCqWVSgULjZtgfOqVX3CBmOonxkr2UB7g",
+        authDomain: "whatsapp-950a8.firebaseapp.com",
+        databaseURL: "https://whatsapp-950a8-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "whatsapp-950a8",
+        storageBucket: "whatsapp-950a8.firebasestorage.app",
+        messagingSenderId: "526342181957",
+        appId: "1:526342181957:web:0e71810f3ccbb297413f2c"
+    },
+    // Telegram Token
+    telegramToken: "YOUR_TELEGRAM_BOT_TOKEN", // Yaha apna bot token daalo
+    // Timezone (India ke liye)
+    timezone: "Asia/Kolkata"
+};
+
